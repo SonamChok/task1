83b855b47bb5a3c8cdbeb90097b48e68368d4372
unknown <02230272.cst@rub.edu.bt>
2024-04-25 01:43:24 -0700

    classwork.py

---
+++ classwork.py
@@ -0,0 +1,12 @@
+class dog:
+    def _init_(self,name,colour,age):
+        self.name=name
+        self.colour=colour
+        self.age=age
+dawa=dog("dawa","red",3)
+nima=dog ("nima", "white",2)
+blacky=dog ("blacky", "black", 1)
+
+print(dawa.name, dawa.colour,dawa.age)
+print(nima.name,nima.colour,nima.age)
+print(blacky.name,blacky.colour,blacky.age)
